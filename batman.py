cafbd502b4cdaf60dbcc64ad1487ee72bef6bbce
Gviox <gviox@visa.com>
2024-08-06 16:06:25 +0000

    learning about dictionaries

---
+++ batman.py
@@ -0,0 +1,13 @@
+batman ={
+    "real name" : "Bruce Wayne",
+    "superpowers" : "billionaire"
+        }
+
+batman.update({"allies": "Robin"})
+batman["allies"] = "superman"
+
+print(batman.keys())
+print(batman.values())
+
+
+
