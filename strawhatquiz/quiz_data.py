14bf3fa6b4ec79e48a36a18fe54c9e4a337c27f7
Gviox <gviox@visa.com>
2024-08-07 00:49:07 +0000

    Have quiz working, need to add exception handling for errors

---
+++ strawhatquiz/quiz_data.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+
+
+questions = [
+    {
+        "question": "How would you tackle a powerful enemy?",
+        "choices": [
+            {"text": "Charge forward with unwavering determination.", "characters": ["Luffy", "Zoro"]},
+            {"text": "Outsmart them with clever tactics.", "characters": ["Nami", "Robin"]},
+            {"text": "Use silly tactics to outsmart them.", "characters": ["Usopp", "Franky"]},
+            {"text": "Rely on your martial arts skills and agility.", "characters": ["Sanji", "Jinbe"]},
+            {"text": "Use your medical knowledge and transform if necessary.", "characters": ["Chopper", "Brook"]}
+        ]
+    },
+    {
+        "question": "What’s your preferred way to explore a new island?",
+        "choices": [
+            {"text": "Stay out of sight from your enemies.", "characters": ["Chopper", "Usopp"]},
+            {"text": "Get lost and dive straight into the adventure to see what happens.", "characters": ["Zoro", "Luffy"]},
+            {"text": "Plan a detailed route and gather all necessary supplies.", "characters": ["Nami", "Robin"]},
+            {"text": "Enjoy the local music and entertainment.", "characters": ["Brook", "Franky"]},
+            {"text": "Make sure your crew doesn't get lost", "characters": ["Sanji", "Jinbe"]}
+        ]
+    },
+    {
+        "question": "What is the first thing you grab to eat at a party?",
+        "choices": [
+            {"text": "Meat.", "characters": ["Luffy", "Brook"]},
+            {"text": "Sake.", "characters": ["Zoro", "Jinbe"]},
+            {"text": "Fresh fruit.", "characters": ["Nami", "Chopper"]},
+            {"text": "Snacks and finger foods.", "characters": ["Usopp", "Robin"]},
+            {"text": "Gourmet dishes.", "characters": ["Sanji", "Franky"]}
+        ]
+    },
+    {
+        "question": "What’s your greatest strength in a battle?",
+        "choices": [
+            {"text": "Unwavering willpower and determination.", "characters": ["Luffy", "Zoro"]},
+            {"text": "Intelligence and navigational skills.", "characters": ["Nami", "Robin"]},
+            {"text": "Creativity and marksmanship.", "characters": ["Usopp", "Franky"]},
+            {"text": "Martial arts.", "characters": ["Sanji", "Jinbe"]},
+            {"text": "Medical knowledge and transformation abilities.", "characters": ["Chopper", "Brook"]}
+        ]
+    },
+    {
+         "question": "How do you handle setbacks?",
+         "choices": [
+             {"text": "Keep moving forward with a smile.", "characters": ["Sanji", "Franky"]},
+             {"text": "Train harder and never give up.", "characters": ["Zoro", "Luffy"]},
+             {"text": "Reassess the situation and adjust your plans.", "characters": ["Nami", "Robin", "Jinbe"]},
+             {"text": "Play it off like you planned for that to happen.", "characters": ["Usopp", "Chopper"]},
+             {"text": "Say you're going to die but realize you’re already dead, 'Yoo hoo hoo!'", "characters": ["Brook"]}
+        ]
+     },
+    {
+        "question": "What’s your ideal team dynamic?",
+        "choices": [
+            {"text": "A crew that supports and trusts each other.", "characters": ["Luffy", "Zoro"]},
+            {"text": "Comrades who train hard and fight together.", "characters": ["Zoro", "Sanji"]},
+            {"text": "A team that listens to plans and follows directions.", "characters": ["Nami", "Robin"]},
+            {"text": "Friends who appreciate and support each other’s talents.", "characters": ["Usopp", "Chopper"]},
+            {"text": "A team that enjoys good food and camaraderie.", "characters": ["Sanji", "Jinbe"]}
+        ]
+    },
+    {
+        "question": "What’s your greatest fear?",
+        "choices": [
+            {"text": "Losing my friends.", "characters": ["Luffy", "Chopper"]},
+            {"text": "Failing to protect those I care about.", "characters": ["Zoro", "Sanji"]},
+            {"text": "Losing my treasure and security.", "characters": ["Nami", "Usopp"]},
+            {"text": "Being unable to save someone.", "characters": ["Chopper", "Robin"]},
+            {"text": "Being forgotten and alone.", "characters": ["Brook", "Franky"]}
+        ]
+    },
+    {
+        "question": "What’s your dream?",
+        "choices": [
+            {"text": "Become The Pirate King.", "characters": ["Luffy"]},
+            {"text": "Become The Greatest Swordsman In The World.", "characters": ["Zoro"]},
+            {"text": "Draw A World Map.", "characters": ["Nami"]},
+            {"text": "Find The All Blue.", "characters": ["Sanji"]},
+            {"text": "Learn All The History Of The World.", "characters": ["Robin"]},
+            {"text": "Be Able To Cure Every Disease.", "characters": ["Chopper"]},
+            {"text": "Be A Great Warrior Of The Sea.", "characters": ["Usopp"]},
+            {"text": "Fishmen and Humans Live Together.", "characters": ["Jinbe"]},
+            {"text": "Build A Ship Capable Of Going Anywhere In The World.", "characters": ["Franky"]},
+            {"text": "Meet Laboon.", "characters": ["Brook"]}
+        ]
+    },
+    {
+        "question": "What’s your fighting style?",
+        "choices": [
+            {"text": "Creative and freely with no limit to one's imagination", "characters": ["Luffy"]},
+            {"text": "Powerful and direct attacks.", "characters": ["Jinbe", "Sanji"]},
+            {"text": "Precision and technique.", "characters": ["Zoro", "Robin"]},
+            {"text": "Clever traps and strategy.", "characters": ["Usopp", "Franky"]},
+            {"text": "Support and teamwork.", "characters": ["Brook", "Nami", "Chopper"]}
+        ]
+    },
+    {
+        "question": "How do you spend your free time?",
+        "choices": [
+            {"text": "Training and improving skills.", "characters": ["Luffy", "Zoro"]},
+            {"text": "Reading, studying, tanning.", "characters": ["Nami", "Robin"]},
+            {"text": "Creating new gadgets or inventions.", "characters": ["Franky", "Usopp"]},
+            {"text": "Cooking and tasting new recipes.", "characters": ["Sanji"]},
+            {"text": "Study and developing new medicine.", "characters": ["Chopper"]},
+             {"text": "Playing music and relaxing.", "characters": ["Brook", "Jinbe"]}
+        ]
+    }
+]
+
+
+
---
+++ strawhatquiz/straw_hat_quiz.py
@@ -0,0 +1,43 @@
+import quiz_data
+from collections import Counter
+
+questions = quiz_data.questions
+
+#This will be used to hav us start at the first question
+question_num = 0
+
+#this will collect the users answers needs to start as an empty array first
+answers = []
+
+# this is for the amount of times a character appears in the selected answer
+character_counts = Counter()
+
+def quiz_questions(questions):
+    global question_num, answers, character_counts
+
+    #display question
+    for question in questions:
+        print(question["question"])
+
+    #display all choices
+        for index, choice in enumerate(question["choices"], 1):
+            print(f"{index}.{choice['text']}")
+
+    #account for user input 
+        user_input = int(input("Choose your answer: ")) - 1
+        selected_choice = question["choices"][user_input]
+        answers.append(user_input + 1)# append the selected number instead of text
+
+    # count the character associated with the selected choice
+        character_counts.update(selected_choice["characters"])
+
+    #increment through the questions by one
+        question_num += 1
+
+quiz_questions(questions)
+
+# find the most common character
+most_common_character = character_counts.most_common(1)[0][0]
+
+# message at the end of quiz telling you which pirate you would be like
+print(f"You would be {most_common_character} from the Straw Hat Pirates")
